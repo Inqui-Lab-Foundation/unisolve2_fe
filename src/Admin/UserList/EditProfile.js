25fff6df29f0a9d36ffe1e1ff98c7e17fdc66687
ramant <ramant@inqui-lab.org>
2023-06-08 18:22:07 +0530

    fixing bugs

--- src/Admin/UserList/EditProfile.js
+++ src/Admin/UserList/EditProfile.js
@@ -39,8 +39,7 @@ const EditProfile = (props) => {
             }
         ]
     };
-    // const phoneRegExp =
-    //     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+    // const phoneRegExp = /^[0-9\s]+$/;
 
     const getValidationSchema = (data) => {
         // where data = mentorData //
