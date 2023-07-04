6299a8083bd5bab1041103d746d3e1abcd374670
Siddu0000 <saisiddardhaa@gmail.com>
2023-07-04 11:06:38 +0530

    Final loginPage

--- src/Routes.js
+++ src/Routes.js
@@ -72,7 +72,7 @@ import ChangePSWModal from './Teachers/ChangePSWModal';
 import Translation from './Admin/Translation/Translation';
 import EditTranslation from './Admin/Translation/EditTranslation';
 import CreateTranslation from './Admin/Translation/CreateTranslation';
-
+import ForgotPasswordNew from './Teachers/ForgotPasswordNew';
 import EditSchool from './Admin/Schools/EditSchool';
 import TeacherEditProfile from './Teachers/EditTeacherProfile';
 
