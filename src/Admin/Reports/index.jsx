79fc71e3bb583056309c42dc8545140afb27e3bc
mesonu-2322 <101508722+mesonu-2322@users.noreply.github.com>
2023-07-20 16:21:52 +0530

    Report Frontend changes are updated

--- src/Admin/Reports/index.jsx
+++ src/Admin/Reports/index.jsx
@@ -1,69 +1,19 @@
 import React from 'react';
 import {Card,Col, Container, Row } from 'reactstrap';
 import Layout from '../Layout';
-//import Cards from './Helpers/Cards';
 import { Link } from 'react-router-dom';
 import './reports.scss';
 
-/*const Reports = () => {
-    // here we can see all reports //
-    const survey = [
-        'Download Students Report'
-        // 'Students Download'
-        // 'Download Teachers Report',
-        // 'Registered Teachers List',
-        // 'Not Registered Teachers List',
-        // 'Teachers Pre Survey',
-        // 'Teachers Course Completion'
-    ];
-    const teacherReports = [
-        'Registered Teachers List',
-        'Not Registered Teachers List',
-        'Teachers Pre Survey Completed List',
-        'Teachers Pre Survey Not Completed List',
-        'Teachers Course Completion List',
-        'Download Teachers Report'
-    ];
-    const districtReports = [
-        'Submitted Challenges',
-        'Draft Challenges',
-        'Accepted Challenges',
-        'Rejected Challenges',
-        'L1 - Yet to Processed Challenges',
-        'L2 - Processed Challenges',
-        'L2 - Yet to  Processed Challenges',
-        'Final Evaluation Challenges',
-        'Final Winner Challenges'
-    ];
-    return(
-        <Layout>
-            <Container className="mt-5 report-wrapper mb-5 pb-5">
-                <h2>Reports</h2>
-                <Row>
-                    <Col md={12}>
-                        <Cards
-                            list={survey}
-                            reports={teacherReports}
-                            distList={districtReports}
-                        />
-                    </Col>
-                </Row>
-            </Container>
-        </Layout>
-    );
-};
-export default Reports;
-*/
 const Reports = () => {
     return (
         <Layout>
             <Container className="mt-5 report-wrapper mb-5 pb-5">
                 <h2>Reports</h2>
-                <div className="reports-data p-3 bg-gray">
+                <div className="reports-data p-5 bg-gray">
                     <Row className="mb-3">
                         <Col lg={6} md={6}>
                             <Link to="/admin/reports-registration">
-                                <Card className="p-4 text-center card-effect mb-3">
+                                <Card className="p-4 text-center card-effect mb-4">
                                     <b className="text-secondary">
                                        REGISTRATION REPORTS
                                     </b>
@@ -74,7 +24,7 @@ const Reports = () => {
                     <Row className="mb-3">
                         <Col lg={6} md={6}>
                             <Link to="/admin/SurveyStatus">
-                                <Card className="p-4 text-center card-effect mb-3">
+                                <Card className="p-4 text-center card-effect mb-4">
                                     <b className="text-secondary">
                                        SURVEY STATUS REPORTS
                                     </b>
@@ -85,7 +35,7 @@ const Reports = () => {
                     <Row className="mb-3">
                         <Col lg={6} md={6}>
                             <Link to="/admin/CourseStatus">
-                                <Card className="p-4 text-center card-effect mb-3">
+                                <Card className="p-4 text-center card-effect mb-4">
                                     <b className="text-secondary">
                                         COURSE STATUS
                                     </b>
@@ -97,7 +47,7 @@ const Reports = () => {
                     <Row className="mb-3">
                         <Col lg={6} md={6}>
                             <Link to="/admin/ChallengesReport">
-                                <Card className="p-4 text-center card-effect mb-3">
+                                <Card className="p-4 text-center card-effect mb-4">
                                     <b className="text-secondary">
                                         CHALLENGES REPORTS
                                     </b>
@@ -108,7 +58,7 @@ const Reports = () => {
                     <Row className="mb-3">
                         <Col lg={6} md={6}>
                             <Link to="/admin/">
-                                <Card className="p-4 text-center card-effect mb-3">
+                                <Card className="p-4 text-center card-effect mb-4">
                                     <b className="text-secondary">
                                         STUDENT AND TEAM STATUS
                                     </b>
@@ -120,7 +70,7 @@ const Reports = () => {
                     <Row className="mb-3">
                         <Col lg={6} md={6}>
                             <Link to="/admin/">
-                                <Card className="p-4 text-center card-effect mb-3">
+                                <Card className="p-4 text-center card-effect mb-4">
                                     <b className="text-secondary">
                                         CERTIFICATES
                                     </b>
