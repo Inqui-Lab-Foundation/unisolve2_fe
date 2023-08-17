d0860fa19ae2a7f4ec8ac49b4e8faed84c8100f2
Arshiya0786 <shaikarshiya@inqui-lab.org>
2023-08-17 14:48:08 +0530

    Footer

--- src/Student/Footer.js
+++ src/Student/Footer.js
@@ -6,10 +6,12 @@ const Footer = () => {
     return (
         <div className="Footer" style={{ backgroundColor: '#fff' }}>
             <Container>
-                <Row className="py-5">
+                <Row className="py-4">
                     <Col md={12}>
                         <p className="p-0 my-auto text-center">
-                            &copy; UNISOLVE, UNICEF {new Date().getFullYear()} <span className="my-auto">&reg;</span> All Rights Reserved.
+                            &copy; UNISOLVE, UNICEF {new Date().getFullYear()}{' '}
+                            <span className="my-auto">&reg;</span> All Rights
+                            Reserved.
                         </p>
                     </Col>
                 </Row>
